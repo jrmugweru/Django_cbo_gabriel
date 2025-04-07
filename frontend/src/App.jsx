5e6f51022bd81ba4cb5c885b7d3488f26c803222
KRISS FORD MWANGI <krissfordmwangi@gmail.com>
2025-04-07 10:51:02 +0300

    Frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 import Donation from "./components/Donation/Donation";
-import Register from "./components/register/Register.jsx";
-import Login from "./components/Login/Login";
+import Register from "./components/Register/Register"
+import LogIn from "./components/Login/LogIn"
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact";
 
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/" element={<Body />} />
         <Route path="/donation" element={<Donation />} />
        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />  
+        <Route path="/login" element={<LogIn />} />  
       </Routes>
       <Contact/>
       <Footer/>
