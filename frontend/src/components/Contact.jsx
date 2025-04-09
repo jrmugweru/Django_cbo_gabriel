abbb917f9f40755f40ec8e940fbec084ed91c424
KRISS FORD MWANGI <krissfordmwangi@gmail.com>
2025-04-09 13:50:06 +0300

    contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
       // }
   
       try {
-        const response = await axios.post("https://django-cbo-gabriel-2.onrender.com/api/register/", {
+        const response = await axios.post("https://django-cbo-gabriel-2.onrender.com/api/contact/", {
           name: formData.name,
           email: formData.email,
           message: formData.message,
